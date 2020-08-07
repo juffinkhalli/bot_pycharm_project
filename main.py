ecc4b4a219b576b67102d228fde4757405e09db2
juffinkhalli <48827066+juffinkhalli@users.noreply.github.com>
2020-08-07 21:44:13 +0300

    boto

--- main.py
+++ main.py
@@ -10,34 +10,39 @@ import os
 token = S3Connection(os.environ['token'])
 url_sbp_dict = S3Connection(os.environ['url_sbp_dict'])
 
+print(token)
+print(type(token))
 
-def get_sbp_dict():
-    url = requests.get(url_sbp_dict)
-    data = url.json()
-    return data['payload']
+print(url_sbp_dict)
+print(type(url_sbp_dict))
 
-
-def chek_sbp():
-    data = get_sbp_dict()
-    for i in data:
-        if 'brand' not in i:
-            error_bank = i['name']
-            return error_bank
-        else:
-            return sbp_members_list()
-
-
-def sbp_members_list():
-    return 'ОК!!!!1'
-
-
-def hello(update, context):
-    update.message.reply_text(chek_sbp())
-
-
-updater = Updater(token, use_context=True)
-
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
-
-updater.start_polling()
-updater.idle()
+# def get_sbp_dict():
+#     url = requests.get(url_sbp_dict)
+#     data = url.json()
+#     return data['payload']
+#
+#
+# def chek_sbp():
+#     data = get_sbp_dict()
+#     for i in data:
+#         if 'brand' not in i:
+#             error_bank = i['name']
+#             return error_bank
+#         else:
+#             return sbp_members_list()
+#
+#
+# def sbp_members_list():
+#     return 'ОК!!!!1'
+#
+#
+# def hello(update, context):
+#     update.message.reply_text(chek_sbp())
+#
+#
+# updater = Updater(token, use_context=True)
+#
+# updater.dispatcher.add_handler(CommandHandler('hello', hello))
+#
+# updater.start_polling()
+# updater.idle()
